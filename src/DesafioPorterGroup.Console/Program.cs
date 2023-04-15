9b4ab2f3afd82ab3dfada2888d9e16cdee5b89f6
Thiago Moreira de Souza Arrais <thiagomds.scientist@gmail.com>
2023-04-15 15:22:09 -0300

    Implementando uma função que recebe uma lista de objetos e retorne uma nova lista apenas com os objetos únicos, ou seja, sem repetições

--- src/DesafioPorterGroup.Console/Program.cs
+++ src/DesafioPorterGroup.Console/Program.cs
@@ -8,3 +8,15 @@ int[] numeros = new int[10] { 1, 2, 3, 4 ,5, 6, 7, 8, 9, 10 };
 Console.WriteLine($"Resultado da somas dos números [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] = {Desafios.SomarValores(numeros)}");
 
 Console.WriteLine($"expressão 2 + 3 * 5 = {Desafios.CalcularExpressaoMatematica("2+3*5")}");
+
+var objetos = new List<Carros>();
+objetos.Add(new Carros() { Fabricante = "Volvo"});
+objetos.Add(new Carros() { Fabricante = "Ferrari"});
+objetos.Add(new Carros() { Fabricante = "Audi"});
+objetos.Add(new Carros() { Fabricante = "Fiat"});
+objetos.Add(new Carros() { Fabricante = "Renaut" });
+objetos.Add(new Carros() { Fabricante = "Fiat" });
+
+
+Console.WriteLine($"Listas objetos \"Carros\" repetidos com retorno da lista" +
+    $" de objetos distintos {Desafios.Exibir(Desafios.ObterListasDistintas(objetos))}");
